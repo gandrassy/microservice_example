197902aab809579ac8831c537fa5f551cdb023a5
Andrássy Gábor <andrassy@mensa.hu>
2023-06-20 07:43:37 +0200

    basic of the project generated by start.spring.io

---
+++ src/test/java/hu/andrassy/microservice_example/MicroserviceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.andrassy.microservice_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
